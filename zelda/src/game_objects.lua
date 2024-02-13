f7f2fdb749626f2c827a13a58d718f2267b317e1
weenyweeny <84559333+weenyweeny@users.noreply.github.com>
2024-02-12 22:59:20 -0800

    zelda hearts

---
+++ zelda/src/game_objects.lua
@@ -0,0 +1,43 @@
+--[[
+    GD50
+    Legend of Zelda
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+]]
+
+GAME_OBJECT_DEFS = {
+    ['switch'] = {
+        type = 'switch',
+        texture = 'switches',
+        frame = 2,
+        width = 16,
+        height = 16,
+        solid = false,
+        defaultState = 'unpressed',
+        consumable = false,
+        states = {
+            ['unpressed'] = {
+                frame = 2
+            },
+            ['pressed'] = {
+                frame = 1
+            }
+        }
+    },
+    ['pot'] = {
+        -- TODO
+    },
+    ['heart'] ={
+        type = 'heart',
+        texture = 'hearts',
+        frame = 5,
+        width = 16,
+        height = 16,
+        solid = false,
+        defaultState = 'unused',
+        consumable = true,
+        states = {['unused'] = {frame = 5}, ['used'] = {frame =1}}
+
+    }
+}
