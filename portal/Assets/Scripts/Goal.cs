90204923c1225e97d303d80bdb274d596499b958
weenyweeny <84559333+weenyweeny@users.noreply.github.com>
2024-04-11 01:37:55 -0700

    portal homework

---
+++ portal/Assets/Scripts/Goal.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    public bool gamewon;
+    //private Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+      	//text = GetComponent<Text>();
+        gamewon = false; 
+		// start text off as completely transparent black
+		//text.color = new Color(0, 0, 0, 0);  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other) {
+        gamewon = true;
+		// trigger coin pickup function if a helicopter collides with this
+		//other.transform.parent.GetComponent<HeliController>().PickupGem();
+		//Destroy(gameObject);
+
+
+			// reveal text only when helicopter is null (destroyed)
+        //text.color = new Color(0, 0, 0, 1);
+        //text.text = "You have done it!";
+        
+        // jump is space bar by default
+        //if (Input.GetButtonDown("Jump")) {
+
+            // reload entire scene, starting music over again, refreshing score, etc.
+       //     SkyscraperSpawner.speed = 10f;
+       //     SceneManager.LoadScene("Main");
+       // }
+    
+    
+    }
+
+}
---
+++ portal/Assets/Scripts/WinText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class WinText : MonoBehaviour
+{
+    public GameObject goal;
+    public GameObject goalpost;
+    private Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+        text.text = "You won!";
+		// start text off as completely transparent black
+		text.color = new Color(0, 0, 0, 0);  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (goal.GetComponent<Goal>().gamewon == true || goalpost.GetComponent<Goal>().gamewon == true){
+		    text.color = new Color(0, 0, 0, 1);  
+        }
+                
+    }
+
+}
