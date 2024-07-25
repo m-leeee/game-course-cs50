f5e33cac3b2d1efe04f862156231424f6e646ed6
weenyweeny <84559333+weenyweeny@users.noreply.github.com>
2024-07-25 14:40:19 -0700

    refactor mech 1,3,4 for timers
    mech 1,3,and 4 were redone with Timer functions instead of a shared timer variable. this allows for doubling up mechanics

--- _finalproj/src/stage1.lua
+++ _finalproj/src/stage1.lua
@@ -40,7 +40,7 @@ function Stage1:init(def)
     
     self.stage:addEnemy(self.boss)
 
-    self.mechanictimer = 0
+    self.mech1counter = 1
     self.mech3counter = 1
     self.mech4counter = 1
 
@@ -48,7 +48,11 @@ end
 
 function Stage1:mech1()
     --TENNIS BALL BARRAGE 
-    if self.mechanictimer > 1 then
+    local t=1
+    if self.mech1counter == 1 then
+        self.mech1counter = self.mech1counter + 1
+        Timer.after(t, function()
+            
         local bullet1= BossBullet{
             boss = self.boss,
 
@@ -123,8 +127,10 @@ function Stage1:mech1()
         self.boss:addBullet(bullet7)
         self.boss:addBullet(bullet8)
 
-        self.mechanictimer = 0
+        self.mech1counter = 1
+        end)
     end
+
 end
 
 function Stage1:mech2() --FIXME:
@@ -153,45 +159,70 @@ end
 
 function Stage1:mech3()
     --FRISBEE BOMB TOSS
-    if self.mechanictimer > 1.3 and self.mech3counter <=2 then
-        local aoe1= AOE{
-            shape = 'circle',
-        
-            --coordinates 
-            x = self.player.hitx,
-            y = self.player.hity,
-        
-            radius = 20, --circle and donut
-        
-            damage = 3, --how much damage this AOE will inflict
-            snaptime = 1, -- time telegraph will show/when the snapshot occurs
-            player = self.player
+    local t = 1.3
+    if self.mech3counter == 1 then
+        Timer.after(t, function()
+            local aoe1= AOE{
+                shape = 'circle',
+            
+                --coordinates 
+                x = self.player.hitx,
+                y = self.player.hity,
+            
+                radius = 20, --circle and donut
+            
+                damage = 3, --how much damage this AOE will inflict
+                snaptime = 1, -- time telegraph will show/when the snapshot occurs
+                player = self.player
 
-        }
-        self.stage:addAOE(aoe1)
+            }
+            self.stage:addAOE(aoe1)
+        end)
         self.mech3counter = self.mech3counter + 1
-        self.mechanictimer = 0
-    elseif self.mechanictimer > 1.3 and self.mech3counter == 3 then
+    elseif self.mech3counter == 2 then
+        Timer.after(t*2, function()
+            local aoe1= AOE{
+                shape = 'circle',
+            
+                --coordinates 
+                x = self.player.hitx,
+                y = self.player.hity,
+            
+                radius = 20, --circle and donut
+            
+                damage = 3, --how much damage this AOE will inflict
+                snaptime = 1, -- time telegraph will show/when the snapshot occurs
+                player = self.player
+    
+            }
+            self.stage:addAOE(aoe1)
+        end)
+        self.mech3counter = self.mech3counter + 1
+    elseif self.mech3counter == 3 then
         local xx = self.player.hitx
         local yy = self.player.hity
-        local aoe1= AOE{
-            shape = 'circle',
-        
-            --coordinates 
-            x = xx,
-            y = yy,
-        
-            radius = 20, --circle and donut
-        
-            damage = 3, --how much damage this AOE will inflict
-            snaptime = 1, -- time telegraph will show/when the snapshot occurs
-            player = self.player
+        Timer.after(t*3, function()
 
-        }
-        self.stage:addAOE(aoe1)
+            xx = self.player.hitx
+            yy = self.player.hity
+            local aoe1= AOE{
+                shape = 'circle',
+            
+                --coordinates 
+                x = xx,
+                y = yy,
+            
+                radius = 20, --circle and donut
+            
+                damage = 3, --how much damage this AOE will inflict
+                snaptime = 1, -- time telegraph will show/when the snapshot occurs
+                player = self.player
+    
+            }
+            self.stage:addAOE(aoe1)
+        end)
         self.mech3counter = self.mech3counter + 1
-
-        Timer.after(1, function()
+        Timer.after((t*3)+1, function()
             local aoe2 = AOE{
                 shape = 'donut',
             
@@ -208,13 +239,10 @@ function Stage1:mech3()
             }
             self.stage:addAOE(aoe2)
         end)
-
-        Timer.after(2, function()
+    
+        Timer.after((t*3)+3, function()
             self.mech3counter = 1
-            self.mechanictimer = 0
         end)
-        
-
     end
 
 
@@ -222,97 +250,102 @@ end
 
 function Stage1:mech4()
     --SANDSTORM 
-    if self.mechanictimer > 0 and self.mech4counter ==1 then
-        local aoe1= AOE{
-            shape = 'triangle',
-        
-            --coordinates 
-            x = VIRTUAL_WIDTH/2,
-            y = VIRTUAL_HEIGHT/2,
-        
-            x2 = 0, --triangle only 
-            y2 = VIRTUAL_HEIGHT, --triangle only   
-            x3 = VIRTUAL_WIDTH, --triangle only
-            y3 = VIRTUAL_HEIGHT, --triangle only
-
-            damage = 3, --how much damage this AOE will inflict
-            snaptime = 2, -- time telegraph will show/when the snapshot occurs
-            player = self.player
-
-        }
-        self.stage:addAOE(aoe1)
+    local t = 1
+    if self.mech4counter == 1 then
         self.mech4counter = self.mech4counter + 1
+        Timer.after(t, function()
+            local aoe1= AOE{
+                shape = 'triangle',
+            
+                --coordinates 
+                x = VIRTUAL_WIDTH/2,
+                y = VIRTUAL_HEIGHT/2,
+            
+                x2 = 0, --triangle only 
+                y2 = VIRTUAL_HEIGHT, --triangle only   
+                x3 = VIRTUAL_WIDTH, --triangle only
+                y3 = VIRTUAL_HEIGHT, --triangle only
     
-    elseif self.mechanictimer > 2 and self.mech4counter == 2 then
-        local aoe1= AOE{
-            shape = 'triangle',
-        
-            --coordinates 
-            x = VIRTUAL_WIDTH/2,
-            y = VIRTUAL_HEIGHT/2,
-        
-            x2 = 0, --triangle only 
-            y2 = 0, --triangle only   
-            x3 = 0, --triangle only
-            y3 = VIRTUAL_HEIGHT, --triangle only
-
-            damage = 3, --how much damage this AOE will inflict
-            snaptime = 1, -- time telegraph will show/when the snapshot occurs
-            player = self.player
-
-        }
-        self.stage:addAOE(aoe1)
-        self.mech4counter = self.mech4counter + 1
+                damage = 3, --how much damage this AOE will inflict
+                snaptime = 1, -- time telegraph will show/when the snapshot occurs
+                player = self.player
     
-    elseif self.mechanictimer > 4 and self.mech4counter == 3 then
-        local aoe1= AOE{
-            shape = 'triangle',
-        
-            --coordinates 
-            x = VIRTUAL_WIDTH/2,
-            y = VIRTUAL_HEIGHT/2,
-        
-            x2 = 0, --triangle only 
-            y2 = 0, --triangle only   
-            x3 = VIRTUAL_WIDTH, --triangle only
-            y3 = 0, --triangle only
-
-            damage = 3, --how much damage this AOE will inflict
-            snaptime = 1, -- time telegraph will show/when the snapshot occurs
-            player = self.player
-
-        }
-        self.stage:addAOE(aoe1)
-        self.mech4counter = self.mech4counter + 1
-    
-    elseif self.mechanictimer > 6 and self.mech4counter == 4 then
-        local aoe1= AOE{
-            shape = 'triangle',
-        
-            --coordinates 
-            x = VIRTUAL_WIDTH/2,
-            y = VIRTUAL_HEIGHT/2,
-        
-            x2 = VIRTUAL_WIDTH, --triangle only 
-            y2 = 0, --triangle only   
-            x3 = VIRTUAL_WIDTH, --triangle only
-            y3 = VIRTUAL_HEIGHT, --triangle only
-
-            damage = 3, --how much damage this AOE will inflict
-            snaptime = 1, -- time telegraph will show/when the snapshot occurs
-            player = self.player
-
-        }
-        self.stage:addAOE(aoe1)
-        self.mech4counter = self.mech4counter + 1
-
-        Timer.after(2, function()
-            self.mech4counter = 1
-            self.mechanictimer = 0
+            }
+            self.stage:addAOE(aoe1)
         end)
+    elseif self.mech4counter == 2 then
+        self.mech4counter = self.mech4counter + 1
+        Timer.after(t*2, function()
+            local aoe1= AOE{
+                shape = 'triangle',
+            
+                --coordinates 
+                x = VIRTUAL_WIDTH/2,
+                y = VIRTUAL_HEIGHT/2,
+            
+                x2 = 0, --triangle only 
+                y2 = 0, --triangle only   
+                x3 = 0, --triangle only
+                y3 = VIRTUAL_HEIGHT, --triangle only
+    
+                damage = 3, --how much damage this AOE will inflict
+                snaptime = 1, -- time telegraph will show/when the snapshot occurs
+                player = self.player
+    
+            }
+            self.stage:addAOE(aoe1)
+        end)
+    elseif self.mech4counter == 3 then
+        self.mech4counter = self.mech4counter + 1
+        Timer.after(t*3, function()
+            local aoe1= AOE{
+                shape = 'triangle',
+            
+                --coordinates 
+                x = VIRTUAL_WIDTH/2,
+                y = VIRTUAL_HEIGHT/2,
+            
+                x2 = 0, --triangle only 
+                y2 = 0, --triangle only   
+                x3 = VIRTUAL_WIDTH, --triangle only
+                y3 = 0, --triangle only    
+    
+                damage = 3, --how much damage this AOE will inflict
+                snaptime = 1, -- time telegraph will show/when the snapshot occurs
+                player = self.player
+    
+            }
+            self.stage:addAOE(aoe1)
+        end)
+    elseif self.mech4counter == 4 then
+        self.mech4counter = self.mech4counter + 1
+        Timer.after(t*4, function()
+            local aoe1= AOE{
+                shape = 'triangle',
+            
+                --coordinates 
+                x = VIRTUAL_WIDTH/2,
+                y = VIRTUAL_HEIGHT/2,
+            
+                x2 = VIRTUAL_WIDTH, --triangle only 
+                y2 = 0, --triangle only   
+                x3 = VIRTUAL_WIDTH, --triangle only
+                y3 = VIRTUAL_HEIGHT, --triangle only
+    
+                damage = 3, --how much damage this AOE will inflict
+                snaptime = 1, -- time telegraph will show/when the snapshot occurs
+                player = self.player
+    
+            }
+            self.stage:addAOE(aoe1)
 
+            Timer.after(t, function()
+                self.mech4counter = 1
+            end)
+    
+        end)
+    end
 
-    end 
 
 end
 
@@ -320,9 +353,8 @@ end
 
 function Stage1:update(dt)
     self.stage:update(dt)
-    self.mechanictimer = self.mechanictimer + dt
 --TODO: phase changes 
-    self.boss.phase = 3 --TODO:FIXME:
+    self.boss.phase = 5 --TODO:FIXME:
     if self.boss.phase == 1 then --TENNIS BALLS BARRAGE
         self:mech1()
     elseif self.boss.phase == 2 then --TRAFFIC
@@ -330,6 +362,9 @@ function Stage1:update(dt)
     elseif self.boss.phase == 3 then --BOMB TOSS
         self:mech3()
     elseif self.boss.phase == 4 then --DUSTSTORM
+        self:mech4()
+    elseif self.boss.phase == 5 then --DUSTSTORM
+        self:mech1()
         self:mech4()
     end
 
